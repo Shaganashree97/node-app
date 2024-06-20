33c66e701ef7d7fcfb1bb0197617fff4009b182e
Shaganashree97 <shaganashree.pl@gmail.com>
2024-06-20 12:23:10 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
     res.json({ msg: "Hello Shagana!" });
 });
 
+// CRUD functionality
+
+// to read
+app.get('/data', () => {})
+
+// to create
+app.post('/data', () => {})
+
+// to update
+app.put('/data/:id', () => {})
+
+// to delete
+app.delete('/data/:id', () => {})
+
+
 app.listen(PORT, () => {
     console.log(`The Server is running at http://localhost:${PORT}`);
 });
